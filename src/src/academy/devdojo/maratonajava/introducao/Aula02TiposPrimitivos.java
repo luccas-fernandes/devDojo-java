4d4a626d5472d3e54226cb83cb72e588aab96f38
luccas-fernandes <fluccas09@gmail.com>
2024-09-26 14:54:21 -0300

    new commit

---
+++ src/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long e boolean.
+        int age = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 20500;
+        byte idadeByte = 12;
+        short idadeShort = 20;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caracter = 'A';
+        System.out.println("a idade é" + age);
+    }
+}
